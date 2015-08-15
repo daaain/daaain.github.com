29aa1f08fe1ee3a6ef777d1e48415f292a0e5fa5
Daniel Demmel <dain@danieldemmel.me>
2015-08-15 14:14:00 +0100

    Site updated at 2015-08-15 13:14:00 UTC

--- javascripts/slash.js
+++ javascripts/slash.js
@@ -1,45 +1,13 @@
-(function($){
-	/* external.js */
-	var host = location.host;
-
-	$('a').on('click', function(e){
-		var href = $(this).attr('href'),
-			link = href.replace(/(https?:\/\/)(.*)\/(.*)/, '$2');
-
-		if (href.match('https?') && link != host){
-			window.open(href);
-			e.preventDefault();
+document.addEventListener('DOMContentLoaded', function(event) {
+	// Image caption + full size
+	var entryNodeList = document.querySelectorAll('.entry img');
+	for (var i = 0; i < entryNodeList.length; ++i) {
+		var image = entryNodeList[i];
+		var alt = image.getAttribute('alt');
+		if (alt !== '' || typeof alt !== 'undefined') {
+			var caption = image.parentNode.insertBefore(document.createElement('span'), image.nextSibling);
+			caption.innerHTML = alt;
+			caption.setAttribute('class', 'caption');
 		}
-	});
-
-	/* navigation.js */
-	var appends = '<option>Menu</option>';
-
-	$('.menu .main > li').each(function(){
-		var link = $(this).children('a');
-		appends += '<option value="'+link.attr('href')+'">'+link.html()+'</option>';
-		$(this).find('li').each(function(){
-			var link = $(this).children('a');
-			appends += '<option value="'+link.attr('href')+'">- '+link.html()+'</option>';
-		});
-	});
-
-	$('nav.menu').append('<select>'+appends+'</select>').on('change', 'select', function(){
-		location.href = $(this).val();
-	});
-
-	/* caption.js */
-	$('.entry').each(function(i){
-		var _i = i;
-		$(this).find('img').each(function(){
-			var alt = $(this).attr('alt');
-
-			if (alt == '' || typeof alt == 'undefined'){
-				$(this).wrap('<a href="'+$(this).attr('src')+'" class="fancybox" rel="gallery'+_i+'" />');
-			} else {
-				$(this).after('<span class="caption">'+alt+'</span>').wrap('<a href="'+$(this).attr('src')+'" class="fancybox" title="'+alt+'" rel="gallery'+_i+'" />');
-			}
-		});
-	});
-	$('.fancybox').fancybox();
-})(jQuery);
+	}
+});
